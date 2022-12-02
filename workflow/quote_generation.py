652f1f11b9aafc0bc175dc284a158b7b8e31c6c5
3v1lkrot666 <doctorstone20@gmail.com>
2022-12-02 10:33:12 +0300

    Updtae

--- workflow/quote_generation.py
+++ workflow/quote_generation.py
@@ -8,16 +8,6 @@ class QuoteGeneration:
         self.parse_data = ParseSites(qSITES)
 
     def generation(self):
-        self.dzhentelmens = "dzhentlmeny-the-gentlemen"
-        self.krestnyi_otec_the_godfather = "krestnyi-otec-the-godfather"
-        self.dzhoker_joker_2012 = "dzhoker-joker-2012"
-        self.neprikasaemye = "1-1-neprikasaemye-the-intouchables"
-        self.matrica_the_matrix = "matrica-the-matrix"
-        self.boicovskii_klub = "boicovskii-klub-fight-club"
-    
-
-
-        
         self.quote_generation = self.parse_data.parse()
         self.summ = len(self.quote_generation)     
         self.number_random = randint(1, self.summ - 1)
